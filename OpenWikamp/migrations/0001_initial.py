5bebdbf802d901af894352059d912303188632af
Konrad Klimczak <konrad.klimczak@comarch.com>
2015-11-20 06:38:28 +0100

    add device width to support mobile devices, small css and html refactoring

--- OpenWikamp/migrations/0001_initial.py
+++ OpenWikamp/migrations/0001_initial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations, models
 from django.conf import settings
 
 
@@ -15,15 +15,25 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Activity',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('title', models.CharField(max_length=200)),
                 ('description', models.TextField()),
             ],
         ),
         migrations.CreateModel(
+            name='ActivityFile',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('expired', models.DateTimeField()),
+                ('file', models.FileField(upload_to='')),
+                ('lesson', models.OneToOneField(related_name='activity', to='OpenWikamp.Activity')),
+            ],
+        ),
+        migrations.CreateModel(
             name='Lesson',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('title', models.CharField(max_length=200)),
                 ('description', models.TextField()),
             ],
@@ -31,7 +41,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('title', models.CharField(max_length=200)),
                 ('text', models.TextField()),
             ],
@@ -39,7 +49,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Schedule',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('date', models.DateField()),
                 ('from_time', models.DateTimeField()),
                 ('to_date', models.DateTimeField()),
@@ -49,7 +59,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Subject',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('title', models.CharField(max_length=200)),
                 ('description', models.TextField()),
                 ('head_teacher', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
