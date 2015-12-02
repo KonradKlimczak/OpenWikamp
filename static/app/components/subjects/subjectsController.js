34bcb587bc0e81084919b5ebbc9b2fb0bb6acaca
Konrad Klimczak <konrad.klimczak@comarch.com>
2015-12-02 22:14:23 +0100

    add delete subjects from list and go to edit subject from list

--- static/app/components/subjects/subjectsController.js
+++ static/app/components/subjects/subjectsController.js
@@ -4,23 +4,38 @@ define([
     controllers.controller('subjectsController', ["$scope", "$http", "$controller",
         function ($scope, $http, $controller) {
 
-        var loadData = function () {
-            $http({
-                method: 'GET',
-                url: '/api/subject/'
-            }).then(function successCallback(response) {
-                $scope.subjects = response.data;
-            }, function(response) {
-                console.log(response);
-                throw new TypeError;
-            })
-        };
+            var loadData = function () {
+                $http({
+                    method: 'GET',
+                    url: '/api/subject/'
+                }).then(function successCallback(response) {
+                    $scope.subjects = response.data;
+                }, function (response) {
+                    console.log(response);
+                    throw new TypeError;
+                })
+            };
 
-        $scope.init = function() {
-            $controller('subjectsState', {$scope: $scope});
-            loadData();
+            var deleteSubject = function (id) {
+                $http({
+                    method: 'DELETE',
+                    url: '/api/subject/' + id
+                }).then(function successCallback(response) {
+                    loadData();
+                }, function errorCallback (response) {
+                    console.log(response);
+                });
+            };
 
-        };
-        $scope.init();
-    }]);
+            $scope.methods = {
+                deleteSubject: deleteSubject
+            };
+
+            $scope.init = function () {
+                $controller('subjectsState', {$scope: $scope});
+                loadData();
+
+            };
+            $scope.init();
+        }]);
 });
